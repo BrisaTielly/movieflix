cf858c87b162eb8f1a7e5d06f4334fe2f5e9babb
BrisaTielly <brisatielly12@gmail.com>
2025-02-11 10:41:32 -0300

    feat: Cria a entidade Movie

---
+++ src/main/java/br/com/movieflix/movieflix/entity/Movie.java
@@ -0,0 +1,73 @@
+package br.com.movieflix.movieflix.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "movie")
+public class Movie {
+
+//    CREATE TABLE movie (
+//    id serial PRIMARY KEY,
+//    name varchar(255) NOT NULL,
+//    description text,
+//    release_date date,
+//    rating numeric,
+//    created_at timestamp,
+//    updated_at timestamp
+//);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    private String description;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    private Double rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    //@JoinTable define a tabela intermediária chamada "movie_category",
+    // que terá duas colunas
+
+    @ManyToMany
+    @JoinTable(name = "movie_category",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories;
+
+    @ManyToMany
+    @JoinTable(name = "movie_streaming",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "streaming_id")
+    )
+    private List<Streaming> streamings;
+
+}
